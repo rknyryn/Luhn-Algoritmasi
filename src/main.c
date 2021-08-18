06c5e993bdd0161615fdf9e71535bc56ace1bb56
Kaan Yarayan <rknyryn@gmail.com>
2021-08-18 17:22:19 +0300

    typo fixed.

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 int main()
 {
     char fileName[255];
-    printf("Okunacak dosyanin uzantisi ile birlikte adini giriniz giriniz (Ornek kullanim: OkunacakDosya.txt): ");
+    printf("Okunacak dosyanin uzantisi ile birlikte adini giriniz (Ornek kullanim: OkunacakDosya.txt): ");
     scanf("%s", fileName);
     read(fileName);
     return 0;
